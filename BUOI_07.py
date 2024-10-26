0f221dbcdcb1196cc9b1152cdff5246635c6e1bb
ImThienz <imthienz@gmail.com>
2024-10-26 16:30:58 +0700

    010100086902-Xử lý ảnh và thị giác máy tính - BUỔI 07

---
+++ BUOI_07.py
@@ -0,0 +1,15 @@
+"Run-length coding"
+# 4 5 3 5 4 3 5 6 3 6 4 3 5 4 6
+# = (1, 4) (4, 5) (3, 6)        "1s 4, 4s 5, 3s 6"
+
+"Run-length coding cải tiến (1)"
+# 4 7 9 7 7 7 7 7 7 7 7 8 8 8 8 8 8
+# = 4 7 9 + 8 7 + 6 8           "(4 7 9 + 8 số 7 + 6 số 8)"
+
+"Run-length coding cải tiến (2)"
+
+"Cây Huffman"
+
+"Mã hóa kiểu LZW"
+
+"Giải nén (đảo ngược)"
